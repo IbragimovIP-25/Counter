0c911fd70ab16ef57fe30ca48f5bd1c068dea1d9
IbragimovIP-25 <cheburashkaena@gmail.com>
2024-10-19 21:19:48 +0300

    решил легкую задачу

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label.text = "Значение счетчика: \(counter)"
     }
-
-
+    override func viewDidDisappear(_ animated: Bool) {
+        counter = 0
+        label.text = "Значение счетчика: 0"
+    }
+    
+    @IBAction func button(_ sender: Any) {
+    counter += 1
+            label.text = "Значение счетчика: \(counter)"
+    }
 }
+ 
 
